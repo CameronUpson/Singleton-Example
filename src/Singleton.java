f1e25867413fc5748c1e171b0782ef9ce8b78d6c
BoneCode <bonecode@live.co.uk>
2015-01-25 17:27:23 -0500

    Singleton Class

---
+++ src/Singleton.java
@@ -0,0 +1,25 @@
+/**
+ * @author Cameron Upson
+ */
+public class Singleton implements Runnable {
+
+	public static volatile Singleton instance = null;
+	
+	public Singleton() {
+		System.out.println("First");
+	}
+	
+	@Override
+	public void run() {
+		System.out.println("Second");
+	}
+	
+	public static synchronized Singleton getInstance() {
+		return instance != null ? instance : (instance = new Singleton());
+	}
+	
+	public static void main(String... args) {
+		new Thread(getInstance()).start();
+	}
+
+}
